77c7902ac2fbe0f366d39a754ae2f66f386a908e
FelipeLuna <fcoladodeluna@gmail.com>
2024-11-20 18:17:39 -0300

    Finalização do header - página home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,24 @@ export default function Home() {
   return (
     <header className={styles.header}>
       <Image
-        src="/assets/logo/Group@3x.png" // Caminho da imagem no diretório `public/`
-        alt="Logo do Grupo" // Texto alternativo obrigatório
+        src="/assets/logo/Group@3x.png"
+        alt="Logo do Grupo"
         layout="intrinsic"
         width={340}
-        height={0} // Ajusta o tamanho com base nas dimensões reais da imagem
+        height={0}
       />
       <h1>EXPLORE O UNIVERSO</h1>
       <h5>Mergulhe no domínio deslumbrante de todos os personagens clássicos que você ama - e aqueles que você descobrirá em breve</h5>
+      <div id="searchInputContainer">
+          <Image
+            src="/assets/busca/Lupa/Shape@1,5x.svg"
+            alt="Logo do Grupo"
+            layout="intrinsic"
+            width={20}
+            height={0}
+          />
+        <input id="searchInput" type="text" placeholder="Procure por heróis"/>
+      </div>
     </header>
   );
 }
